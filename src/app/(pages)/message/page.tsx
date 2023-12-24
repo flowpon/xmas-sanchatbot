21982fac618fc6468cf0537abead10e90df9325a
shun-0810 <132566yamada@gmail.com>
2023-12-24 16:48:50 +0900

    メッセージ画面実装

--- src/app/(pages)/message/page.tsx
+++ src/app/(pages)/message/page.tsx
@@ -1,44 +1,106 @@
-import Image from 'next/image'
+"use client";
+
 import  "./message.css"
+import { useVisualViewportHeightEffect } from "./keybord";
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { getSantaMessage } from "@/app/api/cahtbot";
+
+type MessageBox = {
+  user: string,
+  message: string
+}
 
 export default function Message() {
+  const [userMessage, setUserMessage] = useState("");
+  const [display, setDisplay] = useState<MessageBox[]>([]);
+  const [isBtn, setIsBtn] = useState(false);
+  useVisualViewportHeightEffect();
+
+  function addDisplay(value: MessageBox) {
+    console.log(value)
+    setDisplay((display => 
+      [
+        ...display,
+        {
+          user: value.user,
+          message: value.message
+        }
+      ]))
+  }
+
+  function action() {
+    setUserMessage("");
+    addDisplay({user: "user", message: userMessage});
+    setIsBtn(true);
+    sendMessage(); 
+  }
+
+  async function sendMessage() {
+    const _santaMessage = await getSantaMessage(userMessage);
+    addDisplay({user: "santa", message: _santaMessage});
+    setIsBtn(false);
+  };
+
+  const handleMessageChnage: ChangeEventHandler<HTMLInputElement> = ({
+    target
+  }) => {
+    setUserMessage(target.value);
+  };
+
+  useEffect(() => {
+    submitType()
+  })
+
+  const submitType = () => {
+    if(isBtn) {
+      return (<input type='submit' className='ml-3 bg-slate-400 font-bold text-white px-1.5 h-7 rounded-lg text-sm' disabled />)
+    } else {
+      return (<input type='submit' className='ml-3 bg-blue-800 font-bold text-white px-1.5 h-7 rounded-lg text-sm' />)
+    }
+  }
+  
   return (
     <>
       <header className='bg-green-500 flex justify-center items-center'><img src='/star.png' className='h-12'/><p className='font-desgin px-8'>XmasAI</p></header>
-      <div className='overflow-auto'>
-        <MyMessage />
-        <SantaMessage />
+      <div className='message-body'>
+        {
+          display.map((data, index) => {
+            if(data.user == "santa") {
+              return <SantaMessage key={index} message={data.message}/>
+            } else {
+              return <MyMessage key={index} message={data.message} /> 
+            }
+          })
+        }
       </div>
-      <SendMessage />
+      <form action={action}>
+        <footer>
+          <div className='flex justify-center items-center w-full mt-3 bg-slate-400 h-12'>
+            <input type='text' name="message" className='w-10/12 h-7 rounded-lg' value={userMessage} onChange={handleMessageChnage}/>
+            {submitType()}
+          </div>
+        </footer>
+      </form>
     </>
   )
 }
 
-export function MyMessage() {
+export function MyMessage({message}: any) {
   return(
     <>
-      <div className='my-message-box'><p className='my-message'>ここにユーザーのメッセージが表示されます。</p></div>
+      <div className='my-message-box'><p className='my-message'>{message}</p></div>
     </>
   )
 }
 
-export function SantaMessage() {
+export function SantaMessage({message}: any) {
   return(
     <>
-      <div className='santa-message-box'><p className='santa-message'>ここにサンタからのメッセージが表示されます。</p></div>
+      <div className='santa-message-box'><p className='santa-message'>{message}</p></div>
     </>
   )
 }
 
-export function SendMessage() {
-  return(
-    <>
-      <footer>
-        <div className='flex justify-center items-center w-full mt-3 bg-slate-400 h-12'>
-          <input type='text' className='w-10/12 h-7 rounded-lg' />
-          <input type='submit' className='ml-3 bg-blue-800 font-bold text-white px-1.5 h-7 rounded-lg text-sm' />
-        </div>
-      </footer>
-    </>
-  )
-}
+/**
+ * Sets a var `--visual-viewport-height` to the `<html>` element.
+ */
---
+++ src/app/api/cahtbot.tsx
@@ -0,0 +1,21 @@
+type MessageResponse = {
+    response: string
+}
+
+export async function getSantaMessage(message: string): Promise<string> {
+    console.log(JSON.stringify({ message }));
+    const santaMessage = await fetch("http://116.82.203.135/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", },
+        body: JSON.stringify({ message })
+    })
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+    console.log(santaMessage);
+    if(santaMessage) {
+        return santaMessage.response;
+    } else {
+        return "サンタさん少し休ませて"
+    }
+    
+}
