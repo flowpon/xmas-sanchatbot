1156e76124420e97531229db5efc20c6d24882ad
flowpon <ham42.sh@icloud.com>
2023-12-24 15:26:46 +0900

    css追加と整理、tsx追加と一部変更しました。

--- src/app/(pages)/top/page.tsx
+++ src/app/(pages)/top/page.tsx
@@ -15,27 +15,34 @@ export default function Top() {
           </div>
         </div>
         <div className='main'>
-          <p className='main-text'>クリスマスは、愛と平和のメッセージを伝える日でもあります。<br></br>
+          <p className='main-text'>
+            クリスマス<br></br>
+            愛と平和のメッセージを伝える日<br></br>
             あなたは誰に感謝の気持ちを伝えますか？<br></br>
-            私たちは、あなたにこのメッセージを贈ります。
+            私たちはこのメッセージを<br></br>
+            あなたへ届けます
           </p>
           <p className='sub-text'>
-            あなたにとって暖かく幸せなクリスマスとなりますように。<br></br>
-            そして良い一年をお過ごしください。<br></br>
-            Merry Christmas!!
+            あなたにとって暖かく幸せなクリスマスとなりますように<br></br>
+            そして、良い一年をお過ごしください<br></br>
+            Happy Merry Christmas!!
           </p>
         </div>
         <div className='present'>
           <div className='flex items-center present-box'>
             <img src='/xmaspresent.jpg' className="w-3/5 present-image"/>
             <h2 className="present-text">
+              実は....<br></br>
               私たちもあなたに何か贈りたくて、<br></br>
               クスッと少しでも笑顔になれるような<br></br>
-              ちょっとしたプレゼントを作りました。
+              ちょっとしたプレゼントを作りました。<br></br>
             </h2>
           </div>
+          <a href="#" className="btntextchange">
+            <span>ここを押してね</span>
+            <span>Ho!Ho!Ho!</span>
+          </a>
         </div>
-        <p>AIのやついれる</p>
      </div>
     )
   }
